f69fe5aeb8c64ecb3e7b632ee5a6c36d0024d38a
ruben ballet <ruben.ballet@gmail.com>
2019-02-17 13:13:54 +0100

    Build update

---
+++ Lib/R_LIB.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "C/basic.h"
+#include "ListStructures/ArrayList.h"
+#include "String/String.h"
+#include "TestClasses/Counter.h"
+#include "String/StringPrinter.h"
+#include "Input/KeyboardReader.h"
+#include "Debug/Debug.h"
+#include <vector>
+
+
+int main(int argc, char* argv[]) {
+	std::cout << "test";
+
+	return 0;
+}
